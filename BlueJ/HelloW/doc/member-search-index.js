5630dd0d80061fc07b9320e4c970527ae113482f
kefournier <kfournier@hopkinton.k12.ma.us>
2020-10-01 19:46:00 -0400

    Testing git capablities again

--- BlueJ/HelloW/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"HelloWorld","l":"HelloWorld()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HelloWorld","l":"main(String[])","url":"main(java.lang.String[])"}]
